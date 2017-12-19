42490c0bf59027c2bf9f1981ff791fd127900731
Htoo Eain Lwin <htooeainlwin@rebbiz.com>
2017-12-19 14:14:00 +0630

    db seed update with populate user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+case Rails.env
+when "development"
+  PopulateUsersService.call
+end
