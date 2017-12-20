821edd8857cf9770afa7f0cd024218a833724b7d
Htoo Eain Lwin <htooeainlwin@rebbiz.com>
2017-12-21 00:41:28 +0630

    products routes and spec

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
     constraints subdomain: 'api' do
       devise_for :user
       resources :users, only: [:index, :show], controller: "api/v1/users"
+      resources :products, controller: "api/v1/products"
     end
   end
 end
--- spec/routing/api/v1/routing_spec.rb
+++ spec/routing/api/v1/routing_spec.rb
@@ -3,9 +3,34 @@ include RoutingMacros
 describe API::V1, :type => :routing do
   let(:url_options) { {format: "json", subdomain: "api"} }
 
-  it "routes to /users" do
-    expect(api_get "/users.json").
-      to route_to( "api/v1/users#index", url_options)
+  describe "users" do
+    it "routes to index" do
+      expect(api_get "/users.json").
+        to route_to( "api/v1/users#index", url_options)
+    end
+    it "routes to show" do
+      expect(api_get "/users/1.json").
+        to route_to( "api/v1/users#show", url_options.merge(id: "1"))
+    end
+  end
+
+  describe "products" do
+    it "routes to index" do
+      expect(api_get "/products.json").
+        to route_to( "api/v1/products#index", url_options)
+    end
+    it "routes to show" do
+      expect(api_get "/products/1.json").
+        to route_to( "api/v1/products#show", url_options.merge(id: "1"))
+    end
+    it "routes to update" do
+      expect(api_put "/products/1.json").
+        to route_to( "api/v1/products#update", url_options.merge(id: "1"))
+    end
+    it "routes to destroy" do
+      expect(api_delete "/products/1.json").
+        to route_to( "api/v1/products#destroy", url_options.merge(id: "1"))
+    end
   end
 
 end
