7712592aa11ed135162650f7b1076cbbf69a1a81
Htoo Eain Lwin <htooeainlwin@rebbiz.com>
2017-12-21 22:04:09 +0630

    add inventory level scope in inventory

--- app/models/distribution_center.rb
+++ app/models/distribution_center.rb
@@ -1,4 +1,14 @@
 class DistributionCenter < ApplicationRecord
   validates :name, presence: true
   has_many :warehouses
+  class Singapore
+    def self.warehouses
+      DistributionCenter.find_by(name: "Singapore").warehouses
+    end
+  end
+  class Thailand
+    def self.warehouses
+      DistributionCenter.find_by(name: "Thailand").warehouses
+    end
+  end
 end
--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -6,4 +6,7 @@ class Inventory < ApplicationRecord
   validates :product_id, presence: true
   validates :warehouse_id, presence: true
   validates :count, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
+  scope :from_singapore, -> { where(warehouse_id: DistributionCenter::Singapore.warehouses.pluck(:id))}
+  scope :from_thailand, -> { where(warehouse_id: DistributionCenter::Thailand.warehouses.pluck(:id))}
 end
